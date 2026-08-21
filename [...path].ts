d809c3e8b949c71ecb6d570f0f251ef21f03d116
foxxyoffc <mzakifarel@gmail.com>
2026-08-21 19:36:31 +0800

    Delete [...path].ts

--- [...path].ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { configureApplication } from "../server/_core/app";
-
-// Vercel mengenali berkas dalam folder api sebagai Function. Nama catch-all ini
-// menjaga path asli, misalnya /api/trpc/auth.settings, untuk middleware tRPC.
-const app = configureApplication(express());
-
-export default app;
